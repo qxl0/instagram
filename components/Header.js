8bb0e9454fe08dd9de8c7d41a2dc9e7024ec5580
qiang li <qxl0@yahoo.com>
2022-03-07 20:28:28 -0500

    Added icon

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,18 @@ const Header = () => {
     <div>
       <div className="flex max-w-6xl justify-between bg-white">
         {/* left side */}
-        <div className="relative h-24 w-24">
+        <div className="relative hidden h-24 w-24 lg:inline-grid">
           <Image
             src="https://links.papareact.com/ocw"
             objectFit="contain"
             layout="fill"
+          />
+        </div>
+        <div className="relative h-10 w-10 flex-shrink-0 lg:hidden">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
           />
         </div>
         {/* middle */}
