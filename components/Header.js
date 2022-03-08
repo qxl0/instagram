c2d6daa59420bbb4a2852cde5222ce94fc60622a
qiang li <qxl0@yahoo.com>
2022-03-07 20:20:52 -0500

    configured image domain for Image

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div>
+      {/* left side */}
+      <div>
+        <Image
+          src="https://links.papareact.com/ocw"
+          width={30}
+          height={30}
+          layout="fill"
+        />
+      </div>
+      {/* middle */}
+      {/* right side */}
+    </div>
+  )
 }
 
 export default Header
