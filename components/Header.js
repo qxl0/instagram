6447f906841fc98077641942b226013a67eabe4e
qiang li <qxl0@yahoo.com>
2022-03-07 20:44:00 -0500

    finished searchbox styles

--- components/Header.js
+++ components/Header.js
@@ -13,14 +13,14 @@ const Header = () => {
     <div>
       <div className="flex max-w-6xl justify-between bg-white">
         {/* left side */}
-        <div className="relative hidden h-24 w-24 lg:inline-grid">
+        <div className="relative hidden w-24 lg:inline-grid">
           <Image
             src="https://links.papareact.com/ocw"
             objectFit="contain"
             layout="fill"
           />
         </div>
-        <div className="relative h-10 w-10 flex-shrink-0 cursor-pointer lg:hidden">
+        <div className="relative  w-10 flex-shrink-0 cursor-pointer lg:hidden">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
@@ -28,11 +28,17 @@ const Header = () => {
           />
         </div>
         {/* middle */}
-        <div className="">
-          <div className="absolute  inset-y-0 flex pl-3">
+        <div className="relative mt-1 rounded-md p-3 ">
+          <div className="pointer-events-none  absolute inset-y-0 flex items-center pl-3">
             <SearchIcon className="h-5 w-5 text-gray-500" />
           </div>
-          <input type="text" placeholder="Search" />
+          <input
+            className="block w-full rounded-md border-gray-300 bg-gray-50 
+            pl-10
+            focus:border-black focus:ring-black sm:text-sm"
+            type="text"
+            placeholder="Search"
+          />
         </div>
         {/* right side */}
       </div>
