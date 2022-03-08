9f6562b8ff2d5b94faa392d30a5a12cb9d695014
qiang li <qxl0@yahoo.com>
2022-03-07 20:46:39 -0500

    Add right side

--- components/Header.js
+++ components/Header.js
@@ -8,6 +8,8 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
+
 const Header = () => {
   return (
     <div>
@@ -28,19 +30,22 @@ const Header = () => {
           />
         </div>
         {/* middle */}
-        <div className="relative mt-1 rounded-md p-3 ">
-          <div className="pointer-events-none  absolute inset-y-0 flex items-center pl-3">
-            <SearchIcon className="h-5 w-5 text-gray-500" />
+        <div className="max-w-xs">
+          <div className="relative mt-1 rounded-md p-3 ">
+            <div className="pointer-events-none  absolute inset-y-0 flex items-center pl-3">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="block w-full rounded-md border-gray-300 bg-gray-50 
+              pl-10
+              focus:border-black focus:ring-black sm:text-sm"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input
-            className="block w-full rounded-md border-gray-300 bg-gray-50 
-            pl-10
-            focus:border-black focus:ring-black sm:text-sm"
-            type="text"
-            placeholder="Search"
-          />
         </div>
         {/* right side */}
+        <HomeIcon className="h-10 w-10" />
       </div>
     </div>
   )
