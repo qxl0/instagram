899f8aa6474c190523f2617f8fe77ce79583d92c
qiang li <qxl0@yahoo.com>
2022-03-08 19:51:01 -0500

    Styled the header make it sticky

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ import { HomeIcon } from '@heroicons/react/solid'
 
 const Header = () => {
   return (
-    <div>
+    <div className="sticky top-0 z-50 border-b bg-white shadow-sm">
       <div className="mx-5 flex max-w-6xl justify-between  xl:mx-auto">
         {/* left side */}
         <div className="relative hidden w-24 lg:inline-grid">
