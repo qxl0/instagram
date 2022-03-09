8980e43ac99942653bad32c5c3f60064f0edd8eb
qiang li <qxl0@yahoo.com>
2022-03-08 20:24:32 -0500

    Add Story component

--- components/Stories.js
+++ components/Stories.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker'
+import Story from './Story'
 
 const Stories = () => {
+  const [suggestions, setSuggestions] = useState([])
   useEffect(() => {
     //
     const suggestions = [...Array(20)].map((_, i) => {
       return { ...faker.helpers.contextualCard(), id: i }
     })
-    console.log(suggestions)
+    setSuggestions(suggestions)
   }, [])
-  return <div>Stories</div>
+  return (
+    <div>
+      {suggestions.map((profile) => {
+        return <Story key={profile.id} {...profile} />
+      })}
+    </div>
+  )
 }
 
 export default Stories
---
+++ components/Story.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Story = (props) => {
+  const { username, img } = props
+  console.log({ username, img })
+  return <div>Story</div>
+}
+
+export default Story
