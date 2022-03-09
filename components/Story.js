6cc69b98ffae38f20cf4be973f8e638a0d16c278
qiang li <qxl0@yahoo.com>
2022-03-08 20:41:43 -0500

    Rounded centered

--- components/Story.js
+++ components/Story.js
@@ -4,8 +4,12 @@ const Story = (props) => {
   const { username, avatar: img } = props
   return (
     <div>
-      <img src={img} alt="" />
-      <p>{username}</p>
+      <img
+        src={img}
+        alt=""
+        className="h-14 w-14 rounded-full border-2 border-red-500 object-contain p-[1.5px]"
+      />
+      <p className="w-14 truncate text-center text-xs">{username}</p>
     </div>
   )
 }
