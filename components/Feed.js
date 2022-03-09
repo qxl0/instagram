50544af4897b94b237beda827bca8c1404d572c8
qiang li <qxl0@yahoo.com>
2022-03-08 19:57:58 -0500

    Add Feed, defined the layout of Feed in different screen size

---
+++ components/Feed.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Stories from './Stories'
+
+const Feed = () => {
+  return (
+    <div className="max-auto grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
+      {/* section */}
+      <section>
+        {/* stories */}
+        <Stories />
+        {/* Posts */}
+      </section>
+      {/* section */}
+      <section></section>
+      {/* Mini profile */}
+      {/* suggestions  */}
+    </div>
+  )
+}
+
+export default Feed
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Feed from '../components/Feed'
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       {/* header */}
       <Header />
       {/* feed */}
+      <Feed />
       {/* Modal */}
     </div>
   )
