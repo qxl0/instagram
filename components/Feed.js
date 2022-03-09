f003ef16b17fc6227e63bbee02b24e01cbbfa3ac
qiang li <qxl0@yahoo.com>
2022-03-08 20:08:54 -0500

    Use faker.js to get fake data

--- components/Feed.js
+++ components/Feed.js
@@ -5,7 +5,7 @@ const Feed = () => {
   return (
     <div className="max-auto grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
       {/* section */}
-      <section>
+      <section className="col-span-2">
         {/* stories */}
         <Stories />
         {/* Posts */}
--- components/Stories.js
+++ components/Stories.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { faker } from '@faker-js/faker'
 
 const Stories = () => {
+  useEffect(() => {
+    //
+    const suggestions = [...Array(20)].map((_, i) => {
+      return { ...faker.helpers.contextualCard(), id: i }
+    })
+    console.log(suggestions)
+  }, [])
   return <div>Stories</div>
 }
 
