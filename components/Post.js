06bc630578bea9463169d06f5da4212ae85a1db9
qiang li <qxl0@yahoo.com>
2022-03-09 19:55:11 -0500

    Added flex-1 to push ... to the right

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,26 @@
+import { DotsHorizontalIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 const Post = ({ username, userImg, img, caption }) => {
-  return <div>Post</div>
+  return (
+    <div>
+      {/* header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt=""
+          className="mr-3 h-12 w-12 rounded-full border object-contain p-1"
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+      {/* img */}
+      {/* buttons */}
+      {/* comments */}
+      {/* input box */}
+      <img src={img} alt="" className="h-48 w-48 rounded-full object-cover" />
+    </div>
+  )
 }
 
 export default Post
