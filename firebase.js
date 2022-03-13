474a33575a01b1cd79e0ce6e4d8228118a4ad242
qiang li <qxl0@yahoo.com>
2022-03-13 19:36:03 -0400

    Prepare for firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAf2p7KOgWFjL1M53i4ne4lqNUFDoJj-X8',
+  authDomain: 'insta-test-d2a00.firebaseapp.com',
+  projectId: 'insta-test-d2a00',
+  storageBucket: 'insta-test-d2a00.appspot.com',
+  messagingSenderId: '855942811495',
+  appId: '1:855942811495:web:83cc8ce69a636c907769c2',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
