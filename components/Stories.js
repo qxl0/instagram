018c18d4a0e727c4b12e439f331c0e6cfc6f127d
qiang li <qxl0@yahoo.com>
2022-03-14 20:22:04 -0400

    moidified stories to use session

--- components/Stories.js
+++ components/Stories.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker'
 import Story from './Story'
+import { useSession } from 'next-auth/react'
 
 const Stories = () => {
+  const { data: session } = useSession()
   const [suggestions, setSuggestions] = useState([])
   useEffect(() => {
     //
@@ -17,6 +19,9 @@ const Stories = () => {
        bg-white p-6 scrollbar-thin  
         scrollbar-thin scrollbar-thumb-black"
     >
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
       {suggestions.map((profile) => {
         return <Story key={profile.id} {...profile} />
       })}
