fef5e99fe383da0cb0ad946a09073dba667b2d43
qiang li <qxl0@yahoo.com>
2022-03-14 20:18:45 -0400

    Added conditional styling on session

--- components/Feed.js
+++ components/Feed.js
@@ -8,7 +8,12 @@ import { useSession } from 'next-auth/react'
 const Feed = () => {
   const { data: session } = useSession()
   return (
-    <main className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
+    <main
+      className={`mx-auto grid grid-cols-1 md:max-w-3xl 
+      md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3 ${
+        !session && '!max-w-3xl !grid-cols-1'
+      }`}
+    >
       <section className="col-span-2">
         <Stories />
         <Posts />
