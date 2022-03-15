2da84b9a63e852546b1bab2bc875bf0e72845d0a
qiang li <qxl0@yahoo.com>
2022-03-14 20:16:15 -0400

    No show of minprofile if no session

--- components/Feed.js
+++ components/Feed.js
@@ -3,8 +3,10 @@ import Stories from './Stories'
 import Posts from './Posts'
 import MiniProfile from './MiniProfile'
 import Suggestions from './Suggestions'
+import { useSession } from 'next-auth/react'
 
 const Feed = () => {
+  const { data: session } = useSession()
   return (
     <main className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
       <section className="col-span-2">
@@ -12,12 +14,14 @@ const Feed = () => {
         <Posts />
       </section>
 
-      <section className="hidden md:col-span-1 xl:inline-flex">
-        <div className="fixed top-20">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className="hidden md:col-span-1 xl:inline-flex">
+          <div className="fixed top-20">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   )
 }
