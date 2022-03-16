60abe8050d3fc7560101a468c39a9efb85142ecd
qiang li <qxl0@yahoo.com>
2022-03-15 20:14:46 -0400

    Add @headlessui/react

--- components/Modal.js
+++ components/Modal.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useRecoilState } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
+import { Dialog, Transition } from '@headlessui/react'
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState)
-  console.log('open', open)
-  return <div>{open && <p>THIS Modal Open</p>}</div>
+  return <Transition.Root></Transition.Root>
 }
 
 export default Modal
