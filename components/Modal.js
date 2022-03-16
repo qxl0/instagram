304bfca9b2a2ba963385312d65eb000c7a65ac88
qiang li <qxl0@yahoo.com>
2022-03-15 20:18:56 -0400

    Add Modal dialog

--- components/Modal.js
+++ components/Modal.js
@@ -5,7 +5,17 @@ import { Dialog, Transition } from '@headlessui/react'
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState)
-  return <Transition.Root></Transition.Root>
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        opClose={setOpen}
+      >
+        <div className="flex items-end"></div>
+      </Dialog>
+    </Transition.Root>
+  )
 }
 
 export default Modal
