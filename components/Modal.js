2dc8231557bcac221381e4cf5b2eb83e05609256
qiang li <qxl0@yahoo.com>
2022-03-15 20:25:48 -0400

    Treat the dialog to be placed in center

--- components/Modal.js
+++ components/Modal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useRecoilState } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
 import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState)
@@ -12,7 +13,29 @@ const Modal = () => {
         className="fixed inset-0 z-10 overflow-y-auto"
         opClose={setOpen}
       >
-        <div className="flex items-end"></div>
+        <div
+          className="flex min-h-[800px] items-end justify-center px-4 pt-4 pb-20 
+          text-center sm:block sm:min-h-screen sm:p-0"
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <span
+            className="hidden sm:inline-block sm:h-screen sm:align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+        </div>
       </Dialog>
     </Transition.Root>
   )
