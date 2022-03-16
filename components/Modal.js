3e2517bcced7986c1b903faac80f07a932680e7b
qiang li <qxl0@yahoo.com>
2022-03-15 20:38:55 -0400

    Fixed styling

--- components/Modal.js
+++ components/Modal.js
@@ -39,11 +39,11 @@ const Modal = () => {
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
             leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div
               className="inline-block transform overflow-hidden rounded-lg bg-white px-4
