6abd82940f5fb054de1220297834d41eb34b4468
qiang li <qxl0@yahoo.com>
2022-03-21 21:13:55 -0400

    Still can't load posts

--- components/Posts.js
+++ components/Posts.js
@@ -44,7 +44,14 @@ const Posts = () => {
   return (
     <div>
       {posts.map((post) => (
-        <Post key={post.id} {...post} />
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
+        />
       ))}
     </div>
   )
