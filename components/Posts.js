603402d81b04822ed13f0a6aca396354d599376b
qiang li <qxl0@yahoo.com>
2022-03-21 21:08:21 -0400

    Can't load posts from firebase

--- components/Posts.js
+++ components/Posts.js
@@ -11,22 +11,22 @@ import {
 import { HeartIcon as HearIconFilled } from '@heroicons/react/solid'
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { db } from '../firebase'
-const posts = [
-  {
-    id: '12',
-    username: 'johndoe',
-    userImg: 'https://links.papareact.com/3ke',
-    img: 'https://imageio.forbes.com/specials-images/imageserve/60a77f30768434735cc94924/Grand-Prismatic-Spring---Yellowstone-National-Park/960x0.jpg?fit=bounds&format=jpg&width=960',
-    caption: 'SUBSCRIBE AND DESTORY THE LIKE BUTTON for youtube alogrithm!',
-  },
-  {
-    id: '34',
-    username: 'johndoe',
-    userImg: 'https://links.papareact.com/3ke',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/YellowstonefallJUN05.JPG/1200px-YellowstonefallJUN05.JPG',
-    caption: 'SUBSCRIBE AND DESTORY THE LIKE BUTTON for youtube alogrithm!',
-  },
-]
+// const posts = [
+//   {
+//     id: '12',
+//     username: 'johndoe',
+//     userImg: 'https://links.papareact.com/3ke',
+//     img: 'https://imageio.forbes.com/specials-images/imageserve/60a77f30768434735cc94924/Grand-Prismatic-Spring---Yellowstone-National-Park/960x0.jpg?fit=bounds&format=jpg&width=960',
+//     caption: 'SUBSCRIBE AND DESTORY THE LIKE BUTTON for youtube alogrithm!',
+//   },
+//   {
+//     id: '34',
+//     username: 'johndoe',
+//     userImg: 'https://links.papareact.com/3ke',
+//     img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/YellowstonefallJUN05.JPG/1200px-YellowstonefallJUN05.JPG',
+//     caption: 'SUBSCRIBE AND DESTORY THE LIKE BUTTON for youtube alogrithm!',
+//   },
+// ]
 const Posts = () => {
   const [posts, setPosts] = React.useState([])
   useEffect(
@@ -36,6 +36,7 @@ const Posts = () => {
         query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
         (snapshot) => {
           setPosts(snapshot.docs)
+          console.log(posts)
         }
       ),
     [db]
