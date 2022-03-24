23b3ac1c3b703f69975ec0fb6629d20f1a959795
qiang li <qxl0@yahoo.com>
2022-03-23 20:24:39 -0400

    Moments works nicely

--- components/Post.js
+++ components/Post.js
@@ -17,6 +17,7 @@ import {
   serverTimestamp,
 } from 'firebase/firestore'
 import { db } from '../firebase'
+import Moment from 'react-moment'
 
 const Post = ({ id, username, userImg, img, caption }) => {
   const { data: session } = useSession()
@@ -105,6 +106,10 @@ const Post = ({ id, username, userImg, img, caption }) => {
                 <span className="font-bold">{comment.data().username}</span>{' '}
                 {comment.data().comment}
               </p>
+
+              <Moment fromNow className="pr-5">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
             </div>
           ))}
         </div>
